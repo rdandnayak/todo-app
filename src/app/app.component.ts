afee0d6ff48275680427fb013907a9790e66c4f1
Rohit Dandnayak <rohit.dandnayak@soroco.com>
2022-02-19 22:00:25 +0530

    feat: all functionality completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,40 @@
-import { Component, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
-import { fromEvent, BehaviorSubject, Observable, Subject } from 'rxjs';
-import { map, takeUntil } from 'rxjs/operators';
+import {
+  Component,
+  ViewChild,
+  AfterViewInit,
+  OnDestroy,
+  OnInit,
+  ElementRef,
+  OnChanges,
+} from '@angular/core';
+import {
+  fromEvent,
+  BehaviorSubject,
+  Observable,
+  Subject,
+  combineLatest,
+} from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+  switchMap,
+  take,
+  takeUntil,
+  tap,
+} from 'rxjs/operators';
+import { v1 as uuid } from 'uuid';
+
+enum TodoState {
+  COMPLETED = 'COMPLETED',
+  INCOMPLETE = 'INCOMPLETE',
+}
 
 export interface ITodoItem {
   id: string;
   text: string;
-  state: 'completed' | 'snoozed' | 'progressing';
+  state: TodoState;
 }
 
 export interface IStore {
@@ -17,25 +46,97 @@ export interface IStore {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit, OnDestroy {
+export class AppComponent
+  implements AfterViewInit, OnDestroy, OnInit, OnChanges
+{
   constructor() {}
-  private destroy$: Subject<boolean> = new Subject()
+  public todoSate = TodoState;
+  private destroy$: Subject<boolean> = new Subject();
   public title = 'Todo List App';
-  @ViewChild('todoInput') private todoInput;
-  private store: BehaviorSubject<IStore> = new BehaviorSubject(null);
-  private todosource: Observable<any>;
-  public todoList: Observable<ITodoItem[]> = this.store.pipe(
-    map((store) => store.todoList)
+  private _todoList = new BehaviorSubject([]);
+  public todoList$: Observable<ITodoItem[]> = this._todoList
+    .asObservable()
+    .pipe(tap((list) => (this.todoList = list)));
+  public todoList: ITodoItem[] = [];
+  public remainingTasks$: Observable<number> = this.todoList$.pipe(
+    map((list) =>
+      list.filter((todo) => todo.state === this.todoSate.INCOMPLETE)
+    ),
+    map((items) => items.length)
   );
 
+  public completedTasks$: Observable<number> = this.todoList$.pipe(
+    map((list) =>
+      list.filter((todo) => todo.state === this.todoSate.COMPLETED)
+    ),
+    map((items) => items.length)
+  );
+  @ViewChild('todoInput') private todoInput: ElementRef;
 
+  public ngOnInit() {}
+
+  clearCompletedTasks($event: MouseEvent) {
+    $event.preventDefault();
+    let todoList = this.todoList.filter((todo) => {
+      return todo.state !== this.todoSate.COMPLETED;
+    });
+    this._todoList.next(todoList);
+  }
+  toggleTodo($event) {
+    let result: ITodoItem[];
+    if ($event.target.checked) {
+      result = this.todoList.map((todo) => {
+        if (todo.id === $event.target.value) {
+          todo.state = TodoState.COMPLETED;
+        }
+        return todo;
+      });
+    } else if (!$event.target.checked) {
+      result = this.todoList.map((todo) => {
+        if (todo.id === $event.target.value) {
+          todo.state = TodoState.INCOMPLETE;
+        }
+        return todo;
+      });
+    }
+    this._todoList.next(result);
+  }
+  deleteTodo($event, id: string) {
+    $event.preventDefault();
+    let result: ITodoItem[] = this.todoList.filter((todo) => {
+      return todo.id !== id;
+    });
+    this._todoList.next(result);
+  }
+
+  ngOnChanges() {}
+
+  private handleInputEnter(event) {
+    event.preventDefault();
+    this.todoList.push({
+      id: uuid(),
+      text: event.target.value,
+      state: TodoState.INCOMPLETE,
+    });
+    event.target.value = null;
+    this._todoList.next(this.todoList);
+  }
 
   public ngAfterViewInit() {
-    this.todosource = fromEvent(this.todoInput.nativeElement, 'change').pipe(takeUntil(this.destroy$))
+    fromEvent(this.todoInput.nativeElement, 'keyup')
+      .pipe(
+        debounceTime(150),
+        filter((e: KeyboardEvent) => e.keyCode === 13),
+        distinctUntilChanged(),
+        takeUntil(this.destroy$)
+      )
+      .subscribe((event) => {
+        this.handleInputEnter(event);
+      });
   }
 
   public ngOnDestroy() {
-    this.destroy$.next()
-    this.destroy$.complete()
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
