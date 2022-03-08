41ebfea8350f00ddd67982069bf8afef199c9ab7
Rohit Dandnayak <rohit.dandnayak@soroco.com>
2022-03-08 13:20:50 +0530

    refactor: service is no longer singleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,6 +30,7 @@ import {
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  providers: [ TodoService ]
 })
 export class AppComponent
   implements AfterViewInit, OnDestroy, OnInit, OnChanges
--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,13 +1,7 @@
-import { Injectable, } from '@angular/core';
-import {
-  BehaviorSubject,
-  Observable,
-} from 'rxjs';
-import {
-  map,
-  tap,
-} from 'rxjs/operators';
-import { v1 as uuid } from 'uuid';
+import { Injectable } from '@angular/core'
+import { BehaviorSubject, Observable } from 'rxjs'
+import { map, tap } from 'rxjs/operators'
+import { v1 as uuid } from 'uuid'
 
 export enum TodoState {
   COMPLETED = 'COMPLETED',
@@ -15,88 +9,85 @@ export enum TodoState {
 }
 
 export interface ITodoItem {
-  id: string;
-  text: string;
-  state: TodoState;
+  id: string
+  text: string
+  state: TodoState
 }
 
 export interface IStore {
-  todoList: ITodoItem[];
+  todoList: ITodoItem[]
 }
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class TodoService {
-
-  constructor() { }
-  public todoState = TodoState;
-  private _todoList = new BehaviorSubject([]);
+  constructor() {}
+  public todoState = TodoState
+  private _todoList = new BehaviorSubject([])
   public todoList$: Observable<ITodoItem[]> = this._todoList
     .asObservable()
-    .pipe(tap((list) => (this.todoList = list)));
-  public todoList: ITodoItem[] = [];
+    .pipe(tap((list) => (this.todoList = list)))
+  public todoList: ITodoItem[] = []
   public remainingTasks$: Observable<number> = this.todoList$.pipe(
     map((list) =>
       list.filter((todo) => todo.state === this.todoState.INCOMPLETE)
     ),
     map((items) => items.length)
-  );
+  )
 
   public completedTasks$: Observable<number> = this.todoList$.pipe(
     map((list) =>
       list.filter((todo) => todo.state === this.todoState.COMPLETED)
     ),
     map((items) => items.length)
-  );
+  )
 
   clearCompletedTasks($event: Event) {
-    $event.preventDefault();
+    $event.preventDefault()
     let todoList = this.todoList.filter((todo) => {
-      return todo.state !== this.todoState.COMPLETED;
-    });
-    this._todoList.next(todoList);
+      return todo.state !== this.todoState.COMPLETED
+    })
+    this._todoList.next(todoList)
   }
   toggleTodo($event: Event) {
     const target: HTMLInputElement = $event.target as HTMLInputElement
     console.log($event)
-    let result: ITodoItem[];
+    let result: ITodoItem[]
     if (target.checked) {
       result = this.todoList.map((todo) => {
         if (todo.id === target.value) {
-          todo.state = TodoState.COMPLETED;
+          todo.state = TodoState.COMPLETED
         }
-        return todo;
-      });
+        return todo
+      })
     } else if (!target.checked) {
       result = this.todoList.map((todo) => {
         if (todo.id === target.value) {
-          todo.state = TodoState.INCOMPLETE;
+          todo.state = TodoState.INCOMPLETE
         }
-        return todo;
-      });
+        return todo
+      })
     }
-    this._todoList.next(result);
+    this._todoList.next(result)
   }
   deleteTodo($event: Event, id: string) {
-    $event.preventDefault();
+    $event.preventDefault()
     let result: ITodoItem[] = this.todoList.filter((todo) => {
-      return todo.id !== id;
-    });
-    this._todoList.next(result);
+      return todo.id !== id
+    })
+    this._todoList.next(result)
   }
 
   ngOnChanges() {}
 
   public handleInputEnter(event: Event) {
-    event.preventDefault();
+    event.preventDefault()
     const target = event.target as HTMLInputElement
     this.todoList.push({
       id: uuid(),
       text: target.value,
       state: TodoState.INCOMPLETE,
-    });
-    target.value = null;
-    this._todoList.next(this.todoList);
+    })
+    target.value = null
+    this._todoList.next(this.todoList)
   }
 }
