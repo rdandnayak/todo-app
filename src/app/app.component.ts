760334836841df3cd3da6bff13f1558486f9a4ae
Rohit Dandnayak <rohit.dandnayak@soroco.com>
2022-03-08 12:54:09 +0530

    refactor: refactoring into service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,15 @@ import {
   OnInit,
   ElementRef,
   OnChanges,
-} from '@angular/core';
+} from '@angular/core'
+import { TodoService, TodoState } from './todo.service'
 import {
   fromEvent,
   BehaviorSubject,
   Observable,
   Subject,
   combineLatest,
-} from 'rxjs';
+} from 'rxjs'
 import {
   debounceTime,
   distinctUntilChanged,
@@ -23,23 +24,7 @@ import {
   take,
   takeUntil,
   tap,
-} from 'rxjs/operators';
-import { v1 as uuid } from 'uuid';
-
-enum TodoState {
-  COMPLETED = 'COMPLETED',
-  INCOMPLETE = 'INCOMPLETE',
-}
-
-export interface ITodoItem {
-  id: string;
-  text: string;
-  state: TodoState;
-}
-
-export interface IStore {
-  todoList: ITodoItem[];
-}
+} from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -49,78 +34,18 @@ export interface IStore {
 export class AppComponent
   implements AfterViewInit, OnDestroy, OnInit, OnChanges
 {
-  constructor() {}
-  public todoSate = TodoState;
-  private destroy$: Subject<boolean> = new Subject();
-  public title = 'Todo List App';
-  private _todoList = new BehaviorSubject([]);
-  public todoList$: Observable<ITodoItem[]> = this._todoList
-    .asObservable()
-    .pipe(tap((list) => (this.todoList = list)));
-  public todoList: ITodoItem[] = [];
-  public remainingTasks$: Observable<number> = this.todoList$.pipe(
-    map((list) =>
-      list.filter((todo) => todo.state === this.todoSate.INCOMPLETE)
-    ),
-    map((items) => items.length)
-  );
-
-  public completedTasks$: Observable<number> = this.todoList$.pipe(
-    map((list) =>
-      list.filter((todo) => todo.state === this.todoSate.COMPLETED)
-    ),
-    map((items) => items.length)
-  );
-  @ViewChild('todoInput') private todoInput: ElementRef;
+  constructor(private facade: TodoService) {}
+  public todoList$ = this.facade.todoList$
+  public remainingTasks$ = this.facade.remainingTasks$
+  public completedTasks$ = this.facade.completedTasks$
+  public todoSate = this.facade.todoState
+  public title = 'Todo List App'
+  private destroy$: Subject<boolean> = new Subject()
+  @ViewChild('todoInput') private todoInput: ElementRef
 
   public ngOnInit() {}
 
-  clearCompletedTasks($event: MouseEvent) {
-    $event.preventDefault();
-    let todoList = this.todoList.filter((todo) => {
-      return todo.state !== this.todoSate.COMPLETED;
-    });
-    this._todoList.next(todoList);
-  }
-  toggleTodo($event) {
-    let result: ITodoItem[];
-    if ($event.target.checked) {
-      result = this.todoList.map((todo) => {
-        if (todo.id === $event.target.value) {
-          todo.state = TodoState.COMPLETED;
-        }
-        return todo;
-      });
-    } else if (!$event.target.checked) {
-      result = this.todoList.map((todo) => {
-        if (todo.id === $event.target.value) {
-          todo.state = TodoState.INCOMPLETE;
-        }
-        return todo;
-      });
-    }
-    this._todoList.next(result);
-  }
-  deleteTodo($event, id: string) {
-    $event.preventDefault();
-    let result: ITodoItem[] = this.todoList.filter((todo) => {
-      return todo.id !== id;
-    });
-    this._todoList.next(result);
-  }
-
   ngOnChanges() {}
-
-  private handleInputEnter(event) {
-    event.preventDefault();
-    this.todoList.push({
-      id: uuid(),
-      text: event.target.value,
-      state: TodoState.INCOMPLETE,
-    });
-    event.target.value = null;
-    this._todoList.next(this.todoList);
-  }
 
   public ngAfterViewInit() {
     fromEvent(this.todoInput.nativeElement, 'keyup')
@@ -131,12 +56,22 @@ export class AppComponent
         takeUntil(this.destroy$)
       )
       .subscribe((event) => {
-        this.handleInputEnter(event);
-      });
+        this.facade.handleInputEnter(event)
+      })
+  }
+
+  toggleTodo($event: Event) {
+    this.facade.toggleTodo($event)
+  }
+  deleteTodo($event: Event, id: string) {
+    this.facade.deleteTodo($event, id)
+  }
+  clearCompletedTasks($event: Event) {
+    this.facade.clearCompletedTasks($event)
   }
 
   public ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.complete();
+    this.destroy$.next()
+    this.destroy$.complete()
   }
 }
